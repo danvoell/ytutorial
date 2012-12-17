c033d17b0eb2ac728d6d39f68d577e701b3fb580
Dan Voell <danvoell@gmail.com>
2012-12-16 19:07:08 -0600

    messing around with carrierwave. Improved Pictures. Still not production worthy

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -11,11 +11,11 @@ class ImageUploader < CarrierWave::Uploader::Base
     end
 
     version :thumb do
-      process :resize_to_fill => [200, 200]
+      process :resize_to_fill => [150, 150]
     end
   
     version :medium do
-      process :resize_to_limit => [400, 400]
+      process :resize_to_fill=> [300, 300]
     end
 
   
