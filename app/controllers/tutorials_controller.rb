b1a343d3885b99e1108d09f5a5e116e3fe992c0d
Dan Voell <danvoell@Dans-MacBook-Pro.local>
2013-06-21 12:59:57 -0500

    more styling

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,5 +1,5 @@
 class TutorialsController < ApplicationController
-  before_filter :authenticate_user!, except: [:index, :show]
+  before_filter :authenticate_user!, except: [:index, :show, :tagged]
   before_filter :find_tutorial, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_tutorial, only: [:edit, :update, :destroy]
   
