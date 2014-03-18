7bc8bbf8034246d88df0e695c15b8e020e134edb
PatGW <patrickwalsh@hotmail.co.uk>
2014-03-18 22:18:01 +0000

    fixed logout issue by correcting devise scope in routes.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,6 @@ def show
   @user = User.find(params[:id])
   @tutorials = @user.tutorials
   # raise @user.inspect
-  # @tutorials = Tutorial.find(params[:id])
-  # @tutorials = @user.tutorials
 
 end
 
--- config/routes.rb
+++ config/routes.rb
@@ -5,17 +5,13 @@ Refectory::Application.routes.draw do
     get 'logout', to: "devise/sessions#destroy", as: "logout"
     get 'logout', to: "users/sessions#destroy", as: "logout"
     get 'signup', to: "users#new", as: "signup"
-    match '/users/:id', :to => 'users#show', :as => :user
+    # match '/users/:id', :to => 'users#show', :as => :user
   end
   root :to => 'tutorials#index'
  
 devise_for :users do get '/users/sign_out' => 'devise/sessions#destroy' 
-resources :users
 get 'users/:id' => 'users#show'
 end
-
-
- 
   resources :tutorials
   resources :comments, only: [:show, :create, :update, :destroy]
  
